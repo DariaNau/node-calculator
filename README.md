17a966fc749eea4975b6e98ea50d929858056b35
Daria <dariagnaumova@gmail.com>
2020-08-17 16:15:08 +0500

    Лексанализатор работает, тест калькулятора

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Node.js CLI Calculator 
+
+## App Description
+Command line calculator that runs in Node.js
+
+## TDD
+
+jest
+
+## Instructions
+
+1. First, install node.js: http://nodejs.org/download/
+2. Run the calculator CLI by executing: node index
+3. Start using the calculator!
+
+
+
--- index.js
+++ index.js
@@ -2,10 +2,26 @@ const readline = require("readline");
 const Calculator = require("./lib/Calculator");
 const Lexer = require("./lib/Lexer"),
 
-var rl = readline.createInterface({
+rl = readline.createInterface({
 	input: process.stdin,
 	output: process.stdout
 });
 
 rl.setPrompt("Введите выражение: ");
 rl.prompt();
+let env = {}; // хранение введенных переменных
+
+rl.on('line', function (line) {
+	try {
+		let tokens = new Lexer(line.trim()).tokenize();
+		console.log("=> ", Calculator.evaluate(tokens, env));
+	} catch (e) {
+		console.log("Error:", e);
+	}
+	rl.prompt();
+}).on('SIGINT', function () {
+	rl.close();
+}).on('close', function () {
+	console.log('Have a great day!');
+	process.exit(0);
+});
